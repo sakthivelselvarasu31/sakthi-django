60b108df4ed92f39349f849020a2d649a6152fb8
sakthivel2006 <saktcs152@rmkcet.ac.in>
2025-09-14 11:46:55 +0530

    Update ALLOWED_HOSTS for Render

--- selvarsu/settings.py
+++ selvarsu/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8r2g)#8&dbd-=lpux62efwh3k=+!y5#la+ot%gpz=h9gv#y^=t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['selvarasutextiles2.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 
